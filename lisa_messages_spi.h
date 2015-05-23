8890cd1abb39290f58e57a2c4769746837b0b379
Fabian Girrbach <fabian@hector-SLR>
2015-05-23 18:53:55 +0200

    Adapted messages

--- lisa_messages_spi.h
+++ lisa_messages_spi.h
@@ -16,14 +16,14 @@
 
 /// Footer definition
 typedef struct PACKED{
-    uint32_t crc32;       
+    uint32_t crc32;
 } sensor_data_footer_t;
 
 /// Header definition
 typedef struct PACKED{
-    uint32_t sequence_number;           // Incremented sequence number
-    uint32_t ticks;                     // Number of ticks for timing
-    uint32_t incremented_ticks;
+    uint32_t sequence_number;  // This number is incremented with each new header
+    uint32_t seconds; // Seconds elapsed since start of device
+    uint32_t ticks; // Ticks elapsed since last second
 } sensor_data_header_t;
 
 /// Datatype for VECTOR
@@ -61,6 +61,28 @@ typedef struct PACKED{
     float scaled;
 }sensor_data_airspeed_t ;
 
+typedef struct PACKED {
+  sensor_data_header_t header;
+  uint16_t raw_angle;
+}
+sensor_data_lineangle_t ;
+
+
+//********************************************************************
+/// Actuator MESSAGE DEFINITIONS
+///********************************************************************
+
+typedef struct PACKED{
+    sensor_data_header_t header;
+    sensor_data_header_t set_time;
+    uint32_t flaps_right;
+    uint32_t flaps_left;
+    uint32_t ailerons_right;
+    uint32_t ailerons_left;
+    uint32_t rudder;
+    uint32_t elevator;
+}sensor_data_actuators_t ;
+
 
 //********************************************************************
 /// COMPLETE MESSAGE DEFINITION
@@ -68,7 +90,10 @@ typedef struct PACKED{
 
 typedef struct PACKED{
     sensor_data_header_t header;
-    sensor_data_imu_t imu[NUMBER_OF_IMU_DATA_PACKETS];
+    sensor_data_imu_t imu_plane[NUMBER_OF_IMU_DATA_PACKETS];
+    sensor_data_imu_t imu_arm[NUMBER_OF_IMU_DATA_PACKETS];
+    sensor_data_lineangle_t lineangle1;
+    sensor_data_lineangle_t lineangle2;
     sensor_data_airspeed_t airspeed;
     sensor_data_footer_t footer;
 }sensor_data_t ;
